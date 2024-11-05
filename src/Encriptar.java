60c859e522ea2c12283d15ecf6031050d64e2145
gcronax <guillecampher15@gmail.com>
2024-11-05 19:08:19 +0100

    encriptar

--- src/Encriptar.java
+++ src/Encriptar.java
@@ -1,18 +1,17 @@
 public class Encriptar {
     public static void main(String[] args) {
         String frase =Utilidades.leerCadena("introduce una frase");
-        int count=0;
-
-        for (int i = 1; i < frase.length(); i++) {
-            if (!(frase.charAt(i-1)<(frase.charAt(i)))){
-                count++;
+        String aux = "";
+        char letra;
+        for (int i = 0; i < frase.length(); i++) {
+            letra= ((char) ((int) frase.charAt(i)+2));
+            if (letra=='{'){
+                letra='a';
+            } else if (letra=='|') {
+                letra='b';
             }
-
-
+            aux=aux+letra;
         }
-        if (count>0){
-            System.out.println("no es alfabetico");
-        }else System.out.println("es alfabetico");
-
+        System.out.println(aux);
     }
 }
