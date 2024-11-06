e81d54c052c3fb28ade7d3078cb18aed4dbd2fc9
gcronax <guillecampher15@gmail.com>
2024-11-06 15:46:13 +0100

    Desencriptar

--- src/Desencriptar.java
+++ src/Desencriptar.java
@@ -8,21 +8,21 @@ public class Desencriptar {
             if (97<=frase.charAt(i)&&122>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(123);
                 if (naux<97){
-                    naux=123-naux;
+                    naux=122-(97-naux);
                 }
                 aux=aux+(char)naux;
             }
             if (65<=frase.charAt(i)&&90>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(91);
                 if (naux<65){
-                    naux=90-naux;
+                    naux=90-(65-naux);
                 }
                 aux=aux+(char)naux;
             }
             if (48<=frase.charAt(i)&&57>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(58);
                 if (naux<48){
-                    naux=58-naux;
+                    naux=58-(48-naux);
                 }
                 aux=aux+(char)naux;
             }
