ca9b60ea819958ebbdd33789f5db6188c37d772d
gcronax <guillecampher15@gmail.com>
2024-11-06 15:26:59 +0100

    Desencriptar

--- src/Desencriptar.java
+++ src/Desencriptar.java
@@ -1,29 +1,28 @@
-public class Encriptar {
+public class Desencriptar {
     public static void main(String[] args) {
         String frase =Utilidades.leerCadena("introduce una frase");
         int n=Utilidades.leerEntero("dame un numero");
         String aux = "";
-        char letra = 0;
-
+        n=-n;
         for (int i = 0; i < frase.length(); i++) {
             if (97<=frase.charAt(i)&&122>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(123);
                 if (naux<97){
-                    naux=naux+97;
+                    naux=123-naux;
                 }
                 aux=aux+(char)naux;
             }
             if (65<=frase.charAt(i)&&90>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(91);
                 if (naux<65){
-                    naux=naux+65;
+                    naux=90-naux;
                 }
                 aux=aux+(char)naux;
             }
             if (48<=frase.charAt(i)&&57>=frase.charAt(i)){
                 int naux=((int) frase.charAt(i)+n)%(58);
                 if (naux<48){
-                    naux=naux+48;
+                    naux=58-naux;
                 }
                 aux=aux+(char)naux;
             }
